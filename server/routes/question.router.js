e23da6433f461f6e5ffd07ba6edb5e6915335e1e
Edan Schwartz <edanschwartz@gmail.com>
2020-10-18 21:16:44 -0500

    PUT /question

--- server/routes/question.router.js
+++ server/routes/question.router.js
@@ -27,6 +27,51 @@ router.post('/', rejectUnauthenticated, async (req, res, next) => {
   }
 });
 
+router.put('/:id', rejectUnauthenticated, async(req, res, next) => {
+  const sendMissingError = () => 
+    res.status(404).send({ message: `Question ${req.params.id} does not exist`});
+
+  
+  // Basic input validation
+  for (let key of ['title', 'details']) {
+    if (!req.body.hasOwnProperty(key)) {
+      res.status(400)
+        .json({ message: `missing required field: ${key}`});
+      return
+    }
+  }
+
+  try {
+    if (req.user.authLevel !== 'INSTRUCTOR') {
+      // For students, grab the question first, 
+      // to make sure we access to update.
+      const dbSelectRes = await pool.query(`
+        SELECT * FROM "question" 
+        WHERE question."authorId" = $1;
+      `, [req.user.id]);
+      if (dbSelectRes.rows.length === 0) {
+        return sendMissingError();
+      }
+    }
+
+    // Update the record
+    const dbRes = await pool.query(`
+      UPDATE "question"
+      SET title = $1, details = $2
+      WHERE id = $3
+      RETURNING *;
+    `, [req.body.title, req.body.details, req.params.id]);
+    if (dbRes.rows.length === 0) {
+      return sendMissingError();
+    }
+    res.send(dbRes.rows[0]);
+    return;
+  }
+  catch (err) {
+    next(err);
+  }
+})
+
 router.get('/', rejectUnauthenticated, async(req, res, next) => {
   try {
     const isInstructor = req.user.authLevel === 'INSTRUCTOR';
