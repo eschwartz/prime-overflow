54dbc0f4135aeadc6c0022940db15c1f090c4ccb
Edan Schwartz <edanschwartz@gmail.com>
2020-10-18 20:19:22 -0500

    GET /answer

--- server/routes/answer.router.js
+++ server/routes/answer.router.js
@@ -3,6 +3,31 @@ const pool = require('../modules/pool');
 const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 const router = express.Router();
 
+router.get('/', rejectUnauthenticated, async (req, res, next) => {
+  try {
+    const [queryString, queryParams] = 
+      req.user.authLevel === 'INSTRUCTOR' ?
+        // Instructors can see all answers
+        [`
+          SELECT * FROM "answer";
+        `, []] :
+        // Students may only see answers to questions
+        // from their own cohort
+        [`
+          SELECT "answer".* FROM "answer"
+          JOIN question on answer."questionId" = question.id
+          JOIN "user" on question."authorId" = "user".id
+          WHERE "user"."cohortId" = $1;
+        `, [req.user.cohortId]]
+
+    const dbRes = await pool.query(queryString, queryParams);
+    res.send(dbRes.rows);
+  }
+  catch (err) {
+    next(err);
+  }
+})
+
 router.post('/', rejectUnauthenticated, async (req, res) => {
   try {
     for (let prop of ['questionId', 'details']) {
