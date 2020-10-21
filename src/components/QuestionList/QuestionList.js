b27e3866fbd58f05b057bdf9058821b40e347ca4
Edan Schwartz <edanschwartz@gmail.com>
2020-10-21 16:25:25 -0500

    Material UI lecture

--- src/components/QuestionList/QuestionList.js
+++ src/components/QuestionList/QuestionList.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {Link} from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Container from '@material-ui/core/Container';
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+  paper: {
+    padding: 20,
+    background: 'peachpuff',
+    // .paper h3 { margin-top: 0; }
+    '& h3': {
+      marginTop: 0,
+    },
+    '& h3:hover': {
+      color: 'green',
+      transform: 'rotate(180deg)'
+    }
+  }
+};
+
+
+function MyAwesomeGrid(props) {
+  return (
+    <Grid container spacing={5}>
+      {props.items.map(item => 
+        <Grid item xs={12} sm={6} md={3} key={item.id}>
+          <Paper className={props.classes.paper}>
+            <h3>{item.title}</h3>
+            <div>{item.details}</div>
+          </Paper>
+        </Grid>
+      )}
+    </Grid>
+  )
+}
+
+const StyledGrid = withStyles(styles)(MyAwesomeGrid);
+
 
 class QuestionList extends Component {
 
@@ -19,26 +57,24 @@ class QuestionList extends Component {
   }
 
   render() {
+    const classes = this.props.classes;
+
     return (
-      <>
+      <Container maxWidth="md">
         <h1>Questions Search</h1>
 
-        <ul>
-          {this.props.questions.map(question => 
-            <>
-              <h3>
-                <Link to={`/questions/${question.id}`}>{question.title}</Link>
-              </h3>
-              <div>{this.truncateDetails(question.details, 20)}</div>
-              <div>-- {question.author.fullName} ({question.author.cohort.name})</div>
-            </>
-          )}
-        </ul>
-      </>
+        <StyledGrid 
+          items={this.props.questions}
+        />
+      </Container>
     );
   }
 }
 
+
 export default connect(store => ({
   questions: store.questions
-}))(QuestionList);
+}))(withStyles(styles)(QuestionList));
+
+
+
